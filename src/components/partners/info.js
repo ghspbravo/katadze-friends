0de42c7b3eeff544eff616fb19056ff34ff11d1b
ghspbravo <32223917+ghspbravo@users.noreply.github.com>
2018-08-15 12:53:31 +0500

    partners category fix

--- src/components/partners/info.js
+++ src/components/partners/info.js
@@ -4,7 +4,7 @@ export default partner => {
     return (
         <div className="container">
             <h2 className="text-center">{partner.title}</h2>
-            <h1 className="text-center"><span>{partner.category}</span></h1>
+            <h1 className="text-center"><span>{partner.category.name}</span></h1>
             <img className="col-12" src={partner.img} alt="partnerImg" />
             <p className="v-offset-small">{partner.content}</p>
         </div>
