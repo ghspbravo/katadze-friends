2c1ddb3b10a67a5fab21f679773590fd5e0e51ff
ghspbravo <gmspbravo@gmail.com>
2018-10-04 12:55:38 +0500

    claims fix 2

--- src/components/profile/Claims.js
+++ src/components/profile/Claims.js
@@ -102,8 +102,8 @@ class Claim extends Component {
 
 const mapStateToProps = state => ({
     details: state.claim.info,
-    receiver: state.claim.receiver,
-    sender: state.claim.sender,
+    receiver: state.claim.receiver_info,
+    sender: state.claim.sender_info,
     token: accessToken(state),
 });
 
