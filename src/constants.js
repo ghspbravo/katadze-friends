9400d1ae7da0a5d806e010b5d03401a6fbba1f37
ghspbravo <gmspbravo@gmail.com>
2018-10-19 15:25:03 +0500

    fix redirect uri

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const vkAuthApp = 'https://oauth.vk.com/authorize?client_id=6660131&display=page&redirect_uri=https://katadze.ru&scope=email&response_type=code&v=5.85'
+export const vkAuthApp = 'https://oauth.vk.com/authorize?client_id=6660131&display=page&redirect_uri=https://katadze.ru/login/vk&scope=email&response_type=code&v=5.85'
 
 export const providers = {
     VK: 'vkontakte',
